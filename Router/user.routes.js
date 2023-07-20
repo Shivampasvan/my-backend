6446b1fabbb4ed58a44e61af144f29d8c25214cb
Shivampasvan <shivampasvan@gmail.com>
2023-07-20 18:39:18 +0530

    basic done

---
+++ Router/user.routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const userRouter = express.Router();
+const { UserModel } = require("../Model/user.model");
+
+userRouter.get("/get", async (req, res) => {
+  try {
+    const user = await UserModel.find();
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+userRouter.post("/add", async (req, res) => {
+  try {
+    const { fname, lname, mail, country, state, city, gender, dob, age } =
+      req.body;
+    const user = new UserModel({
+      fname,
+      lname,
+      mail,
+      country,
+      state,
+      city,
+      gender,
+      dob,
+      age,
+    });
+    await user.save();
+    res.status(200).send("User's data added to the server !!");
+  } catch (error) {
+    res.status(400).send(error.msg);
+  }
+});
+
+module.exports = {
+  userRouter,
+};
---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const { connection } = require("./Config/connection");
+const { userRouter } = require("./Router/user.routes");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send(
+    '<h1 style="text-align:center; color:Blue;">Welcome To Frequent Backend</h1>'
+  );
+});
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log(`Server is Running at ${process.env.port} `);
+  } catch (error) {
+    console.log(error);
+  }
+});
