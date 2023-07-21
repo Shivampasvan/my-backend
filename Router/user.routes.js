8d78785a31653859b12123e79d076ff033f00101
Shivampasvan <shivampasvan@gmail.com>
2023-07-21 06:22:16 +0530

    update

--- Router/user.routes.js
+++ Router/user.routes.js
@@ -12,6 +12,10 @@ userRouter.get("/get", async (req, res) => {
 });
 
 userRouter.post("/add", async (req, res) => {
+
+  const {mail} = req.body
+  const useremailfound = await UserModel.find({mail})
+  if (useremailfound.length === 0){
   try {
     const { fname, lname, mail, country, state, city, gender, dob, age } =
       req.body;
@@ -30,6 +34,8 @@ userRouter.post("/add", async (req, res) => {
     res.status(200).send("User's data added to the database !!");
   } catch (error) {
     res.status(400).send(error.msg);
+  }}else{
+    res.status(401).send("User Already Registered with this email");
   }
 });
 
